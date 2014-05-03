483fe0347ebdd7e42653c0a4e96637145becc2a1
leonardoleal <leonardo@lealweb.com.br>
2014-05-03 01:20:29 -0300

    [Pilha] Add app ConversorBinario

--- Pilha/src/com/senac/app/ConversosBinario.java
+++ Pilha/src/com/senac/app/ConversosBinario.java
@@ -1,5 +1,42 @@
 package com.senac.app;
 
+import static java.lang.System.*;
+import java.util.Scanner;
+
+import com.senac.estruturas.Stack;
+
 public class ConversosBinario {
 
+	public static int dec2bin(int decimal) {
+		Stack<String> pilha = new Stack<String>();
+
+        while (decimal != 0) {
+            if (decimal % 2 == 1)
+                pilha.push("1");
+
+            else
+                pilha.push("0");
+
+            decimal /= 2;
+        }
+
+        String binary = "0";
+        while (!pilha.isEmpty()) {
+			binary += pilha.pop();
+			
+		}
+
+		return Integer.parseInt(binary);
+    }
+
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner(in);
+    	
+    	out.println("Entre com o valor decimal: ");
+    	int input = scanner.nextInt();
+
+		out.println("Valor em binário: " + dec2bin(input));
+
+		scanner.close();
+	}
 }
