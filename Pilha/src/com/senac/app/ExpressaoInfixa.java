3c66da0d3951b5b9f4a57d46d47cfef6732dc432
leonardoleal <leonardo@lealweb.com.br>
2014-05-20 00:18:06 -0300

    [Pilha] Expressão infixa com algarismos > 10

--- Pilha/src/com/senac/app/ExpressaoInfixa.java
+++ Pilha/src/com/senac/app/ExpressaoInfixa.java
@@ -10,8 +10,9 @@ public class ExpressaoInfixa {
     public static String infixa2posfixa(String infixa) {
     	Stack<Character> pilha = new Stack<Character>();
     	String posfixa = "";
+    	char chAnterior = ' ';
 
-    	for (char ch : infixa.toCharArray()){
+    	for (Character ch : infixa.toCharArray()){
 			if (ch == '(') {
 				pilha.push(ch);
 
@@ -22,50 +23,75 @@ public class ExpressaoInfixa {
 						break;
 					}
 					posfixa += pilha.pop();
+					posfixa += " ";
 				}
 
 			} else if ("*/".indexOf(ch) >= 0) {
 				if (!pilha.isEmpty()
-						&& "(+-".indexOf(pilha.peek()) < 0)
+						&& "(+-".indexOf(pilha.peek()) < 0) {
 					posfixa += pilha.pop();
+					posfixa += " ";
+				}
 				pilha.push(ch);
 
 			} else if ("+-".indexOf(ch) >= 0) {
 				if (!pilha.isEmpty()
-						&& pilha.peek() != '(')
+						&& pilha.peek() != '(') {
 					posfixa += pilha.pop();
+					posfixa += " ";
+				}
 				pilha.push(ch);
 
 			} else if (Character.isDigit(ch)) {
+				if (Character.isDigit(chAnterior)) {
+					posfixa = posfixa.trim();
+				}
+				
 				posfixa += ch;
+				posfixa += " ";
 			}
+
+			chAnterior = ch;
     	}
 
 		while (!pilha.isEmpty()) {
 			posfixa += pilha.pop();
+			posfixa += " ";
 		}
 
-		return posfixa;
+		return posfixa.trim();
     }
 
     public static double resolveExpInfixa(String infixa) {
     	Stack<Double> pilha = new Stack<Double>();
     	String posfixa = infixa2posfixa(infixa);
+    	char chAnterior = ' ';
     	double arg2;
 
     	for (char ch : posfixa.toCharArray()){
     		if (Character.isDigit(ch)) {
+				if (Character.isDigit(chAnterior)) {
+					pilha.push(
+							Double.parseDouble(
+									String.format("%.0f%c", pilha.pop(), ch)
+							)
+					);
+					continue;
+				}
+
 				pilha.push(
 						Double.parseDouble(
 								Character.toString(ch)
 						)
 				);
-			} else {
+			} else if (ch != ' ') {
 				arg2 = pilha.pop();
 				pilha.push(
 						execOperacao(pilha.pop(), arg2, ch)
 				);
 			}
+
+			chAnterior = ch;
     	}
 
     	return Double.parseDouble(
--- Pilha/src/com/senac/app/test/ExpressaoInfixaTest.java
+++ Pilha/src/com/senac/app/test/ExpressaoInfixaTest.java
@@ -10,17 +10,32 @@ public class ExpressaoInfixaTest {
 
 	@Test
 	public void testEntradaInfixaSaidaPosfixa() {
-    	String infixa = "( 4 + 5 / ( 6 + 9 ) ) + 7 * 8";
+    	String infixa = "(4 + 5 /( 6 +99 ) ) + 7* 8";
 
 		String posfixa = ExpressaoInfixa.infixa2posfixa(infixa);
-		assertEquals("4569+/+78*+", posfixa);
+		assertEquals("4 5 6 99 + / + 7 8 * +", posfixa);
 	}
 
 	@Test
-	public void testResolveExpressaoInfixa() {
+	public void testResolveExpressaoInfixaAlgarismosMenorQueDez() {
     	String infixa = "( 6 * 8 / ( 6 * 9 - 6) + 4 ) + 7 * 8 - 2";
-    	// 68*69*6-/4+78*2+-
+    	// 6 8 * 6 9 * 6 - / 4 + 7 8 * 2 - +
 
 		assertEquals(59, ExpressaoInfixa.resolveExpInfixa(infixa), 0);
+	}
+
+	@Test
+	public void testResolveExpressaoInfixaAlgarismosMaiorQueDez() {
+    	String infixa = "( 6 * 8 / ( 6 * 9 - 6) + 10 ) + 7 * 8 - 22";
+    	// 6 8 * 6 9 * 6 - / 4 + 7 8 * 22 - +
+
+		assertEquals(45, ExpressaoInfixa.resolveExpInfixa(infixa), 0);
+	}
+
+	@Test
+	public void testResolveExpressaoInfixaResultadoNegativo() {
+    	String infixa = "( 6 * 8 / ( 6 * 9 - 6) + 10 ) - 545 + 7 * 8 - 22";
+
+		assertEquals(-500, ExpressaoInfixa.resolveExpInfixa(infixa), 0);
 	}
 }
